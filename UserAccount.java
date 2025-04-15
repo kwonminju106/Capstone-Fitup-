8dcb6bd14c11fd068fba31c960c1b50ded4e53ca
kwonminju106 <144650750+kwonminju106@users.noreply.github.com>
2025-04-16 00:59:58 +0900

    Add files via upload

---
+++ UserAccount.java
@@ -0,0 +1,36 @@
+package com.example.myapplication123;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserAccount {
+
+    public UserAccount() {
+        
+    }
+    public void setIdToken() {
+    }
+
+    private void idToken() {
+    }
+
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+
+    private String idToken;
+
+    public String getEmailId() { return emailId; }
+
+    public void setEmailId(String emailId) { this.emailId = emailId; }
+
+    private String emailId;
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password = password; }
+
+    private String password;
+}
